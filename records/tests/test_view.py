8a8942e001a700b659943da8208925b1189fc3b6
zhougit86 <zhou16386@163.com>
2016-10-26 16:24:43 +0800

    delete the add new_item method and URL, add judgement in the view_list

--- records/tests/test_view.py
+++ records/tests/test_view.py
@@ -98,6 +98,18 @@ class NewListTest(TestCase):
 
 # class NewItemTest(TestCase):
 class ListViewTest(TestCase):
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
     def test_can_save_a_POST_request_to_an_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()

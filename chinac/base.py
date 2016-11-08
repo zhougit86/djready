d448e52ff97aab1e20d040c77914b7ca908d0bd2
Xiaogang <zhouxiaogang@chinac.com>
2016-11-08 13:44:24 +0800

    changed

--- chinac/base.py
+++ chinac/base.py
@@ -1,7 +1,9 @@
+# -*- coding:utf-8 -*-
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import sys
-
+import time
+from selenium.webdriver.support.select import Select
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -38,3 +40,31 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def create_depart(self):
+        button = self.browser.find_element_by_xpath("//a[@href='/platformenterprise/create/']")
+        button.click()
+        # self.browser.session_id()
+        # for i in list:
+        #     print i.get_attribute('data-object-id')
+        time.sleep(5)
+
+        parent = self.browser.find_element_by_id('id_parent')
+        Select(parent).select_by_visible_text('PMO')
+        self.browser.find_element_by_id('id_name').send_keys('test_depart')
+        self.browser.find_element_by_id('fromuserlist').click()
+
+        time.sleep(1)
+        self.browser.find_element_by_id("user_table_div").find_element_by_class_name('form-control').send_keys(
+            'testrole')
+        time.sleep(1)
+        users = self.browser.find_element_by_id("user_table")
+        users.find_element_by_name("btSelectItem").click()
+
+        # self.browser.find_element_by_tag_name()
+        div = self.browser.find_element_by_class_name('modal-footer')
+        div.find_element_by_xpath("//input[@value='确认提交']").click()
+
+        time.sleep(10)
+        print self.browser.current_url
+
--- chinac/test_login.py
+++ chinac/test_login.py
@@ -3,10 +3,12 @@
 from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.action_chains import ActionChains
 import unittest
 import time
 
-user_list={'user1':'passw0rd'}
+user_list={'admin':'1qazxsw2)('}
 
 
 class NewVisitorTest(FunctionalTest):
@@ -16,16 +18,41 @@ class NewVisitorTest(FunctionalTest):
         time.sleep(5)
 
     # def test_click_compute_host_list(self):
-        self.browser.find_element_by_link_text("计算").click()
-        time.sleep(1)
-        list=self.browser.find_element_by_xpath("//a[@href='/project/compute/']")
-        # print list.get_attribute('href')
-        list.click()
-        time.sleep(5)
-        list=self.browser.find_element_by_id('instances').find_elements_by_tag_name('tr')
-        self.browser.session_id()
-        for i in list:
-            print i.get_attribute('data-object-id')
+    #     self.browser.find_element_by_link_text("计算").click()
+    #     time.sleep(1)
+    #     button=self.browser.find_element_by_xpath("//a[@href='/platformenterprise']")
+    #     # print list.get_attribute('href')
+    #     button.click()
+    #     time.sleep(8)
+    #     self.create_depart()
+
+
+        # def expand(nodes):
+        #     for i in nodes:
+        #         child = i.find_element_by_xpath("//i[@role='presentation']")
+        #         print i.get_attribute('aria-expanded')
+        #         child.click()
+        #         time.sleep(2)
+        #         node_in = self.browser.find_elements_by_xpath("//li[@aria-expanded='false']")
+        #         expand(node_in)
+        #
+        #
+        # expand(self.browser.find_elements_by_xpath("//li[@aria-expanded='false']"))
+        # print self.browser.find_element_by_link_text("总部").get_attribute('id')
+        # ActionChains(self.browser).double_click(self.browser.find_element_by_link_text("总部")).perform()
+        # time.sleep(1)
+        # ActionChains(self.browser).move_to_element(self.browser.find_element_by_link_text("PMO")).perform()
+        # ActionChains(self.browser).double_click(self.browser.find_element_by_link_text("PMO")).perform()
+        # time.sleep(1)
+
+
+
+
+
+
+
+
+
 
 
 
